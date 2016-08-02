6dbeab3ac19a9fe34da337b34b13553a8d9cb2ff
Deepak Negi <Deepak.Negi@Copart.com>
2016-08-02 12:05:20 -0500

    prep repo for Heroku

--- server.js
+++ server.js
@@ -2,11 +2,24 @@ var express = require('express');
 
 //create our app
 var app = express(); // call express lib as a function passing nothing as argument
+const PORT = process.env.PORT || 3000;
 
+// req might be index.html or bundle.js
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto' == 'http']){
+        next(); // it will let the req process as normal
+    } else {
+        res.redirect('http://'+req.hostname+req.url);
+    }
+});
 //app.use let us add functionality to our express application
 //express.static is used to specify the folder name that we wanna expose to the web server
 app.use(express.static('public'));
 // app.listen(to start the server) takes 2 args, port and the function which will be called when the server is up.
-app.listen(3000, function(){
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+    console.log('Express server is up on port ' + port);
 });
+
+// start in package.json is to tell heroku where to start from
+// We are only using port 3000, but heroku will tell us which port
+// we should be binding to using an environment variable.
