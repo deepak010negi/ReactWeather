2e0c26f9334c790027b1fbb7da69d24a140ed880
Deepak Negi <Deepak.Negi@Copart.com>
2016-08-02 20:27:11 -0500

    Style About component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,19 @@ var React = require('react');
 var About = React.createClass ({
   render: function(){
     return (
-      <h3>About Component</h3>
+      <div>
+        <h1 className="text-center">About</h1>
+        <p>This is the weather application built on react</p>
+        <p>Here are some tools which I have used</p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather by city name.
+          </li>
+        </ul>
+      </div>
     );
   }
 });
